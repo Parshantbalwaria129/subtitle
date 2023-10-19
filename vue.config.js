2dd90c075aaa93100eac6c6fe045211c7622e9a8
parshant <parshantbalwaria@gmail.com>
2023-10-20 01:24:10 +0530

    ready to reploy

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  // Need an extra config to avoid Vue.js (v3) mixed content error when opened from Gitpod in a browser (see https://github.com/gitpod-io/gitpod/issues/11170)
-  devServer: {
-    allowedHosts: "all",
-    client: {
-    //   // Reference : https://webpack.js.org/configuration/dev-server/#websocketurl
-      webSocketURL: "auto://172.16.5.4:8080/ws",
-    },
-  },
+  publicPath: "/subtitle/",
 });
